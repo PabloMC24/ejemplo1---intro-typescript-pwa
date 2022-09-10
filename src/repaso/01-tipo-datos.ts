076fd850a481a65c537ae33c409250ef6928d1e8
PabloMC24 <managedcloth@hotmail.com>
2022-09-10 09:49:23 -0500

    ejemplo1

---
+++ src/repaso/01-tipo-datos.ts
@@ -0,0 +1,18 @@
+
+
+console.log('Hola Mundo! -pwd-1003');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre:string ="Pablo"
+let cont:number=111;
+let acredita:boolean=false;
+
+let apaterno: number | string='juan';
+apaterno=238;
+
+var miVariable:string='Hola';
+const tem:number=234
+
