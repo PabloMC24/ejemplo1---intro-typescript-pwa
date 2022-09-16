c74d2a6a508bbe9b9515cb7e2780d675f8dcf4eb
PabloMC24 <managedcloth@hotmail.com>
2022-09-15 22:59:39 -0500

    ejemplo4

--- src/repaso/practica01.ts
+++ src/repaso/practica01.ts
@@ -32,3 +32,40 @@ const pruebaAlumno:utlAlumno={
 }
 
 console.log(pruebaAlumno.mostrarDireccion);
+
+
+
+//
+
+interface Dirrecion{
+    calle: string
+    pais:string
+    estado:string
+}
+
+interface DatosAlumno{
+    nombre:string;
+    edad:number;
+    direccion:direccion;
+    mostrarDireccion:()=>string
+}
+
+
+const AlumnoUTL:DatosAlumno={
+    nombre:'Pablo',
+    edad:22,
+    direccion:{
+        calle:'Colines',
+        pais:'México',
+        estados:'Guanajuato',
+        numero:217
+
+    },
+ mostrarDireccion(){
+    return this.nombre +','+this.direccion.estados+','+this.direccion.pais
+}
+
+}
+
+const direccion=AlumnoUTL.mostrarDireccion();
+console.log(direccion)
